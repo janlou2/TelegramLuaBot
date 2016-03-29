6f527c2da549d83f97070528d89ac2d7f5e5bc40
janlou2 <atefehazimi94@gmail.com>
2016-03-29 11:13:53 +0430

    Update Block.lua

--- plugins/Block.lua
+++ plugins/Block.lua
@@ -15,7 +15,7 @@ local triggers = {
 		return -- End if the sender is blacklisted.
 	end
 
-	if not string.match(msg.text_lower, '^/blacklist') then
+	if not string.match(msg.text_lower, '^لیست سیاه') then
 		return true
 	end
 
